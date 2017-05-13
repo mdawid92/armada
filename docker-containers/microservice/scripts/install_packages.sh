d177997d97d8629b42c6f120987a91f014d2bdb8
krise3k <krise3k@gmail.com>
2017-05-13 14:14:31 +0200

    install less

--- docker-containers/microservice/scripts/install_packages.sh
+++ docker-containers/microservice/scripts/install_packages.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y --no-install-recommends supervisor python python-dev python-pip curl mc \
+apt-get install -y --no-install-recommends supervisor python python-dev python-pip curl mc less\
     software-properties-common wget gcc unzip apt-utils net-tools cron netcat
 add-apt-repository -y ppa:vbernat/haproxy-1.7
 apt-get update
